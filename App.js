8c629aef9c8f2205ea491c643d0bbd5e09f6515e
Sairam Nulakani <sairamnulakani4@gmail.com>
2023-09-01 02:02:18 +0530

    created bottom tab navigator

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
+import BottomTabNavigator from "./navigation/BottomTabNavigator";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    regular: require("./assets/fonts/Poppins-Regular.ttf"),
+    light: require("./assets/fonts/Poppins-Light.ttf"),
+    bold: require("./assets/fonts/Poppins-Bold.ttf"),
+    medium: require("./assets/fonts/Poppins-Medium.ttf"),
+    extrabold: require("./assets/fonts/Poppins-ExtraBold.ttf"),
+    semibold: require("./assets/fonts/Poppins-SemiBold.ttf"),
+  });
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello,</Text>
-      <Text style={styles.name}>Sairam Nulakani</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Bottom Navigation"
+          component={BottomTabNavigator}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -19,6 +50,7 @@ const styles = StyleSheet.create({
     padding: 40,
   },
   name: {
-    fontWeight: "bold",
+    fontFamily: "extrabold",
+    fontSize: 20,
   },
 });
--- screens/Home.jsx
+++ screens/Home.jsx
@@ -2,14 +2,14 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-const Search = () => {
+const Home = () => {
   return (
     <SafeAreaView>
-      <Text>Search</Text>
+      <Text>Home</Text>
     </SafeAreaView>
   );
 };
 
-export default Search;
+export default Home;
 
 const styles = StyleSheet.create({});
