53df57af898d3ac0e8c1c9733de7da1f317be251
Sairam Nulakani <sairamnulakani4@gmail.com>
2023-09-01 23:35:15 +0530

    created schema for products

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,14 @@ const app = express();
 
 mongoose
   .connect(
-    `mongodb+srv://sairamnulakani4:${process.env.MONGO_PASSWORD}@cluster0.bzwhuop.mongodb.net/?retryWrites=true&w=majority`
+    `mongodb+srv://sairamnulakani4:${process.env.MONGO_PASSWORD}@cluster0.bzwhuop.mongodb.net/furniture?retryWrites=true&w=majority`
   )
   .then(() => {
     console.log("Connected to DB");
   })
   .catch((err) => console.log(err));
+
+app.get("/", (req, res) => res.status(200).send("Hello"));
 
 app.listen(8090, () => {
   console.log("Server Running");
